cb8d4be245df14726c1545014048e47d8748974c
HFSTeam <105112369+HFSTeam@users.noreply.github.com>
2022-08-31 13:56:45 +0700

    Website interface

---
+++ script.js
@@ -0,0 +1,4 @@
+var obj1 = {"video": {
+    "value": "<iframe title='Video Của Team' type=\"text/html\" width='640' height='390' src='https://www.youtube.com/embed/B08mIWEb_08' frameborder='0' allowFullScreen></iframe>"
+}}
+document.write(obj1.video.value);
